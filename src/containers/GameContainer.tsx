218221ecc63ddb361356614cffef2c2ee664040b
고태진 <thereisnotruth12@gmail.com>
2023-03-10 02:34:17 +0900

    drag

--- src/containers/GameContainer.tsx
+++ src/containers/GameContainer.tsx
@@ -1,22 +1,82 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import GamePage from '../pages/GamePage';
 
 const GameContainer = () => {
+  const boundaryRef = useRef<HTMLDivElement>(null);
   const [isDragged, setIsDragged] = useState<boolean>(false);
+  const [startX, setStartX] = useState<number>(0);
+  const [startY, setStartY] = useState<number>(0);
+  const [endX, setEndX] = useState<number>(0);
+  const [endY, setEndY] = useState<number>(0);
+  const [width, setWidth] = useState<number>(0);
+  const [height, setHeight] = useState<number>(0);
+  const [boundLeft, setBoundLeft] = useState<number>(0);
+  const [boundRight, setBoundRight] = useState<number>(0);
+  const [boundTop, setBoundTop] = useState<number>(0);
+  const [boundBottom, setBoundBottom] = useState<number>(0);
   const handleMouseDown = (event: React.MouseEvent) => {
-    console.log(event);
-    setIsDragged(true);
+    if (
+      event.clientX < boundRight &&
+      event.clientX > boundLeft &&
+      event.clientY < boundBottom &&
+      event.clientY > boundTop
+    ) {
+      setIsDragged(true);
+      setEndX(event.clientX);
+      setEndY(event.clientY);
+      setStartX(event.clientX);
+      setStartY(event.clientY);
+    }
   };
   const handleMouseUp = (event: React.MouseEvent) => {
-    console.log(event);
     setIsDragged(false);
+    setWidth(0);
+    setHeight(0);
   };
+
+  const handleMouseMove = (event: React.MouseEvent) => {
+    if (
+      event.clientX < boundRight &&
+      event.clientX > boundLeft &&
+      event.clientY < boundBottom &&
+      event.clientY > boundTop
+    ) {
+      if (isDragged) {
+        if (event.clientX - endX > 0) {
+          setWidth(event.clientX - startX);
+        } else {
+          setStartX(event.clientX);
+          setWidth(endX - event.clientX);
+        }
+        if (event.clientY - endY > 0) {
+          setHeight(event.clientY - startY);
+        } else {
+          setStartY(event.clientY);
+          setHeight(endY - event.clientY);
+        }
+      }
+    }
+  };
+  useEffect(() => {
+    if (boundaryRef.current) {
+      setBoundLeft(boundaryRef.current.offsetLeft);
+      setBoundRight(boundaryRef.current.offsetLeft + boundaryRef.current.clientWidth);
+      setBoundTop(boundaryRef.current.offsetTop);
+      setBoundBottom(boundaryRef.current.offsetTop + boundaryRef.current.clientHeight);
+    }
+  }, []);
   return (
     <GamePage
+      boundaryRef={boundaryRef}
       isDragged={isDragged}
       handleMouseDown={handleMouseDown}
       handleMouseUp={handleMouseUp}
+      handleMouseMove={handleMouseMove}
+      startX={startX}
+      startY={startY}
+      width={width}
+      height={height}
     />
   );
 };
--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,19 +1,50 @@
 import React from 'react';
-
 import { FaAppleAlt } from 'react-icons/fa';
 
 const GamePage = (props: any) => {
-  const { isDragged, handleMouseDown, handleMouseUp } = props;
+  const {
+    boundaryRef,
+    isDragged,
+    handleMouseDown,
+    handleMouseUp,
+    handleMouseMove,
+    startX,
+    startY,
+    width,
+    height,
+  } = props;
+
   return (
-    <div className="bg-green h-full">
+    <div
+      className="bg-green h-full select-none"
+      onMouseUp={handleMouseUp}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseUp}
+    >
       <div className="h-1/6">헤더</div>
       <div
         className="h-4/6 w-5/6 bg-light_green mx-auto rounded-xl max-w-6xl"
-        onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}
+        ref={boundaryRef}
       ></div>
-      <div className="h-1/6">푸터</div>
-      <div className={isDragged ? '' : 'hidden'}>드래그</div>
+      <div className="h-1/6"></div>
+      <div
+        style={
+          isDragged
+            ? {
+                position: 'absolute',
+                boxSizing: 'border-box',
+                border: '0.5px solid black',
+                top: startY,
+                left: startX,
+                width: width,
+                height: height,
+              }
+            : {
+                display: 'hidden',
+              }
+        }
+      ></div>
     </div>
   );
 };
