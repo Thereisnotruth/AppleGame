b3edeaa33569f8ad49f60e346a75423750a7cb06
고태진 <thereisnotruth12@gmail.com>
2023-03-09 17:58:03 +0900

    main, game page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import MainPage from './pages/MainPage';
+import MainContainer from './containers/MainContainer';
+import GameContainer from './containers/GameContainer';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<MainContainer />} />
+        <Route path="/game" element={<GameContainer />} />
       </Routes>
     </div>
   );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaAppleAlt } from 'react-icons/fa';
 
-const MainPage = () => {
-  return <div>mainpage</div>;
+const MainPage = (props: any) => {
+  const { startGame } = props;
+  return (
+    <div className="w-full h-full bg-green flex justify-center content-center flex-wrap">
+      <div className="w-3/4 h-3/4 bg-light_green rounded-xl flex justify-center items-center">
+        <div className="flex flex-col items-center w-1/2">
+          <FaAppleAlt className="text-red text-[200px] my-3" />
+          <button
+            type="button"
+            className="my-5 bg-green w-full py-4 text-white text-2xl rounded-xl"
+            onClick={startGame}
+          >
+            게임시작
+          </button>
+          <button
+            type="button"
+            className="my-5 bg-green w-full py-4 text-white text-2xl rounded-xl"
+          >
+            랭킹
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
