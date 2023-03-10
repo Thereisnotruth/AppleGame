4983557175902859c81c84b4e487e938254445cc
고태진 <thereisnotruth12@gmail.com>
2023-03-10 23:48:56 +0900

    game

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
+import { FaAppleAlt } from 'react-icons/fa';
 
 const GamePage = (props: any) => {
   const {
     boundaryRef,
-    isSelected,
     isDragged,
     handleMouseDown,
     handleMouseUp,
     handleMouseMove,
     startX,
     startY,
-    endX,
-    endY,
     width,
     height,
-    direction,
     score,
-    drag,
-    reset,
+    moveMain,
     time,
     createApple,
+    isGameOver,
+    tryAgain,
   } = props;
 
   return (
@@ -31,7 +29,7 @@ const GamePage = (props: any) => {
       onMouseLeave={handleMouseUp}
     >
       <div className="h-1/6 w-5/6 flex items-center mx-auto justify-around mx-auto">
-        <div className="w-4/6 h-1/6 bg-light_green rounded-xl">
+        <div className="w-4/6 h-1/6 bg-light_green rounded-xl mx-w-6xl">
           <div
             className="h-full bg-red rounded-xl"
             style={{
@@ -43,18 +41,18 @@ const GamePage = (props: any) => {
         <div className="w-1/6 bg-light_green py-4 text-3xl rounded-xl">{score}</div>
       </div>
       <div
-        className="h-4/6 w-5/6 bg-light_green mx-auto rounded-xl max-w-6xl flex justify-center items-center flex-wrap"
+        className="h-4/6 w-5/6 bg-light_green rounded-xl max-w-6xl flex justify-center items-center flex-wrap mx-auto"
         ref={boundaryRef}
       >
         {createApple()}
       </div>
-      <div className="h-1/6 flex items-center w-5/6 mx-auto">
+      <div className="h-1/6 flex items-center w-5/6 mx-auto max-w-6xl">
         <button
           type="button"
           className="bg-light_green py-4 px-9 text-2xl rounded-xl"
-          onClick={reset}
+          onClick={moveMain}
         >
-          초기화
+          메인으로
         </button>
       </div>
       <div
@@ -73,7 +71,43 @@ const GamePage = (props: any) => {
                 display: 'hidden',
               }
         }
-      ></div>
+      />
+      <div
+        className="absolute top-0 z-10 w-full h-full bg-trans_gray flex justify-center items-center"
+        style={isGameOver ? {} : { display: 'none' }}
+      >
+        <div className="bg-green w-2/6 rounded-xl p-[10px]">
+          <div className="flex-wrap flex-col bg-light_green rounded-xl w-full py-6 flex justify-center items-center">
+            <div className="w-1/2 justify-center flex items-center font-bold text-3xl">
+              <FaAppleAlt className="text-red text-6xl" />
+              게임 오버...
+            </div>
+            <div className="w-1/2 flex text-2xl justify-center">{score}점</div>
+            <input
+              type="text"
+              className="w-1/2 text-center text-2xl bg-light_green outline-none border-none my-4"
+              style={{ borderBottom: '0.5px solid black' }}
+              placeholder="닉네임 입력..."
+            />
+            <div className="w-1/4 flex flex-col">
+              <button
+                type="button"
+                className="bg-green w-full text-xl rounded-xl py-2 text-white my-1"
+                onClick={moveMain}
+              >
+                랭킹등록(미구현)
+              </button>
+              <button
+                type="button"
+                className="bg-green w-full text-xl rounded-xl py-2 text-white my-1"
+                onClick={tryAgain}
+              >
+                다시하기
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
