c83a9b04a8b8928684342fc038d98fb3909bbc57
고태진 <thereisnotruth12@gmail.com>
2023-03-11 15:56:34 +0900

    logo

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -9,7 +9,7 @@ const MainPage = (props: any) => {
     <div className="w-full h-full bg-green flex justify-center content-center flex-wrap">
       <div className="w-3/4 h-3/4 bg-light_green rounded-xl flex justify-center items-center">
         <div className="flex flex-col items-center w-1/2">
-          <FaAppleAlt className="text-red xl:text-[200px] text-[60px] my-3" />
+          <FaAppleAlt className="text-red xl:text-[200px] text-[80px] lg:text-[120px] my-3" />
           <button
             type="button"
             className="my-5 bg-green w-full xl:py-4 text-xl py-2 text-white xl:text-2xl rounded-xl"
